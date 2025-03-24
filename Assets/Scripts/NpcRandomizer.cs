25bdcba580ee6bcf5ac8ebb999b18076ca5cf8ef
FerMod <tudelaigarcia@gmail.com>
2025-03-24 17:26:33 +0100

    Extract extension class to file

--- Assets/Scripts/NpcRandomizer.cs
+++ Assets/Scripts/NpcRandomizer.cs
@@ -19,19 +19,6 @@ public class NpcRandomizer : MonoBehaviour
     }
 }
 
-static class ArrayExtension
-{
-    public static T Random<T>(this T[] array)
-    {
-        return array[array.RandomIndex()];
-    }
-
-    public static int RandomIndex<T>(this T[] array)
-    {
-        return UnityEngine.Random.Range(0, array.Length);
-    }
-}
-
 [Serializable]
 public class PartList
 {
